b56ebf04e55c7b41512a2c53a7fc6462c6458157
Joe Navratil <navratil@gmail.com>
2015-02-28 07:32:55 -0800

    Updating gh-pages with the Carbonific's lists; no PR for this.

--- setdex_nuggetBridge.js
+++ setdex_nuggetBridge.js
@@ -1,89 +1,142 @@
 var SETDEX_NUGGETBRIDGE={
-  "Charizard": {
-    "Level 51's Turn↑TheHeat": {
+  "Tyranitar": {
+    "Metatyph6's Winter 2015 Darude": {
       "level": 50,
       "evs": {
-        "hp": "252",
-        "df": "4",
-        "sa": "44",
-        "sd": "20",
-        "sp": "188"
+        "hp": "196",
+        "sa": "252",
+        "sp": "60"
       },
       "nature": "Modest",
-      "ability": "Solar Power",
-      "item": "Charizardite Y",
+      "ability": "Sand Stream",
+      "item": "Tyranitarite",
       "moves": [
-        "Heat Wave",
-        "Solar Beam",
-        "Overheat",
+        "Ice Beam",
+        "Rock Slide",
+        "Fire Blast",
         ""
       ]
     }
   },
-  "Cresselia": {
-    "Level 51's Crescenda": {
+  "Excadrill": {
+    "Metatyph6's Winter 2015 Guacamole": {
       "level": 50,
       "evs": {
-        "hp": "220",
-        "df": "172",
-        "sa": "4",
-        "sd": "92",
-        "sp": "20"
+        "at": "252",
+        "df": "4",
+        "sp": "252"
       },
-      "nature": "Calm",
-      "ability": "Levitate",
+      "nature": "Adamant",
+      "ability": "Sand Rush",
       "item": "",
       "moves": [
-        "Psyshock",
-        "Ice Beam",
+        "Earthquake",
+        "Iron Head",
+        "Rock Slide",
+        ""
+      ]
+    },
+    "Fatum's Winter 2015 Dornröschen": {
+      "level": 50,
+      "evs": {
+        "hp": "4",
+        "at": "252",
+        "sp": "252"
+      },
+      "nature": "Adamant",
+      "ability": "Mold Breaker",
+      "item": "",
+      "moves": [
+        "Earthquake",
+        "Rock Slide",
         "",
         ""
       ]
     }
   },
-  "Mamoswine": {
-    "Level 51's SWINE FLU++": {
-      "level": 50,
-      "evs": {
-        "at": "252",
-        "sd": "4",
-        "sp": "252"
-      },
-      "nature": "Adamant",
-      "ability": "Thick Fat",
-      "item": "Choice Scarf",
-      "moves": [
-        "Icicle Crash",
-        "Earthquake",
-        "Iron Head",
-        "Rock Slide"
-      ]
-    }
-  },
-  "Ferrothorn": {
-    "Level 51's Free Hugs": {
+  "Cradily": {
+    "Metatyph6's Winter 2015 CrayCrayLovr": {
       "level": 50,
       "evs": {
         "hp": "252",
-        "at": "148",
-        "sd": "108"
+        "df": "252",
+        "sd": "4"
       },
-      "ivs": {
-        "sp": "0"
-      },
-      "nature": "Relaxed",
-      "ability": "",
-      "item": "Leftovers",
+      "nature": "Bold",
+      "ability": "Storm Drain",
+      "item": "Sitrus Berry",
       "moves": [
-        "Gyro Ball",
-        "Power Whip",
+        "Giga Drain",
+        "Ancient Power",
+        "",
+        ""
+      ]
+    }
+  },
+  "Talonflame": {
+    "Metatyph6's Winter 2015 Bravest Bird": {
+      "level": 50,
+      "evs": {
+        "at": "252",
+        "sa": "44",
+        "sp": "212"
+      },
+      "nature": "Adamant",
+      "ability": "",
+      "item": "Life Orb",
+      "moves": [
+        "Brave Bird",
+        "Overheat",
         "",
         ""
       ]
     }
   },
   "Thundurus": {
-    "Level 51's Thundurus": {
+    "Metatyph6's Winter 2015 Voltergeist": {
+      "level": 50,
+      "evs": {
+        "sa": "252",
+        "sd": "4",
+        "sp": "252"
+      },
+      "ivs": {
+        "at": "30",
+        "df": "30"
+      },
+      "nature": "Timid",
+      "ability": "",
+      "item": "Safety Goggles",
+      "moves": [
+        "Thunderbolt",
+        "Hidden Power Ice",
+        "",
+        ""
+      ]
+    },
+    "Guru Fernandez's Winter 2015 Voltolos": {
+      "level": 50,
+      "evs": {
+        "hp": "244",
+        "df": "140",
+        "sa": "36",
+        "sd": "68",
+        "sp": "20"
+      },
+      "ivs": {
+        "sp": "30"
+      },
+      "nature": "Bold",
+      "ability": "",
+      "item": "Sitrus Berry",
+      "moves": [
+        "Thunderbolt",
+        "Hidden Power Ice",
+        "",
+        ""
+      ]
+    },
+    "Level 51's Winter 2015 Thundurus": {
       "level": 50,
       "evs": {
         "hp": "236",
@@ -106,8 +159,316 @@ var SETDEX_NUGGETBRIDGE={
       ]
     }
   },
+  "Aegislash": {
+    "Metatyph6's Winter 2015 Blade Works": {
+      "level": 50,
+      "evs": {
+        "hp": "252",
+        "at": "4",
+        "sa": "252"
+      },
+      "ivs": {
+        "sp": "0"
+      },
+      "nature": "Quiet",
+      "ability": "",
+      "item": "",
+      "moves": [
+        "Shadow Ball",
+        "Sacred Sword",
+        "Flash Cannon",
+        ""
+      ]
+    }
+  },
+  "Metagross": {
+    "Guru Fernandez's Winter 2015 REGIS MK-V": {
+      "level": 50,
+      "evs": {
+        "hp": "156",
+        "at": "156",
+        "sp": "196"
+      },
+      "nature": "Jolly",
+      "ability": "Clear Body",
+      "item": "Metagrossite",
+      "moves": [
+        "Iron Head",
+        "Zen Headbutt",
+        "Ice Punch",
+        ""
+      ]
+    }
+  },
+  "Hydreigon": {
+    "Guru Fernandez's Winter 2015 Trigun": {
+      "level": 50,
+      "evs": {
+        "hp": "28",
+        "df": "36",
+        "sa": "244",
+        "sd": "4",
+        "sp": "196"
+      },
+      "nature": "Modest",
+      "ability": "Levitate",
+      "item": "Choice Specs",
+      "moves": [
+        "Dark Pulse",
+        "Draco Meteor",
+        "Fire Blast",
+        "Earth Power"
+      ]
+    }
+  },
+  "Milotic": {
+    "Guru Fernandez's Winter 2015 Murmaider": {
+      "level": 50,
+      "evs": {
+        "hp": "236",
+        "df": "156",
+        "sa": "100",
+        "sd": "12",
+        "sp": "4"
+      },
+      "nature": "Bold",
+      "ability": "",
+      "item": "Leftovers",
+      "moves": [
+        "Scald",
+        "Icy Wind",
+        "",
+        ""
+      ]
+    },
+    "Fatum's Winter 2015 Rapunzel": {
+      "level": 50,
+      "evs": {
+        "hp": "12",
+        "df": "4",
+        "sa": "252",
+        "sd": "4",
+        "sp": "236"
+      },
+      "nature": "Timid",
+      "ability": "",
+      "item": "Choice Scarf",
+      "moves": [
+        "Scald",
+        "Ice Beam",
+        "Hydro Pump",
+        ""
+      ]
+    }
+  },
+  "Terrakion": {
+    "Guru Fernandez's Winter 2015 Oxford": {
+      "level": 50,
+      "evs": {
+        "hp": "4",
+        "at": "252",
+        "sp": "252"
+      },
+      "nature": "Jolly",
+      "ability": "",
+      "item": "Lum Berry",
+      "moves": [
+        "Rock Slide",
+        "Close Combat",
+        "",
+        ""
+      ]
+    }
+  },
+  "Venusaur": {
+    "Guru Fernandez's Winter 2015 Bulbasage": {
+      "level": 50,
+      "evs": {
+        "hp": "180",
+        "df": "76",
+        "sa": "60",
+        "sd": "132",
+        "sp": "60"
+      },
+      "nature": "Calm",
+      "ability": "Chlorophyll",
+      "item": "Venusaurite",
+      "moves": [
+        "Giga Drain",
+        "Sludge Bomb",
+        "",
+        ""
+      ]
+    }
+  },
+  "Togekiss": {
+    "Fatum's Winter 2015 Schneeweißchen": {
+      "level": 50,
+      "evs": {
+        "hp": "236",
+        "df": "196",
+        "sa": "4",
+        "sd": "12",
+        "sp": "60"
+      },
+      "nature": "Bold",
+      "ability": "",
+      "item": "Sitrus Berry",
+      "moves": [
+        "Air Slash",
+        "",
+        "",
+        ""
+      ]
+    }
+  },
+  "Conkeldurr": {
+    "Fatum's Winter 2015 Meistergriff": {
+      "level": 50,
+      "evs": {
+        "hp": "212",
+        "at": "44",
+        "df": "140",
+        "sd": "108",
+        "sp": "4"
+      },
+      "ivs": {
+        "sp": "13"
+      },
+      "nature": "Adamant",
+      "ability": "Guts",
+      "item": "Assault Vest",
+      "moves": [
+        "Drain Punch",
+        "Mach Punch",
+        "Stone Edge",
+        "Knock Off"
+      ]
+    }
+  },
+  "Scizor": {
+    "Fatum's Winter 2015 Rosenrot": {
+      "level": 50,
+      "evs": {
+        "hp": "28",
+        "at": "252",
+        "sp": "228"
+      },
+      "nature": "Adamant",
+      "ability": "Technician",
+      "item": "Life Orb",
+      "moves": [
+        "Bullet Punch",
+        "Bug Bite",
+        "Knock Off",
+        ""
+      ]
+    }
+  },
+  "Salamence": {
+    "Fatum's Winter 2015 Aschenputtel": {
+      "level": 50,
+      "evs": {
+        "hp": "4",
+        "at": "252",
+        "sp": "252"
+      },
+      "nature": "Jolly",
+      "ability": "Intimidate",
+      "item": "Salamencite",
+      "moves": [
+        "Double-Edge",
+        "Earthquake",
+        "",
+        ""
+      ]
+    }
+  },
+  "Charizard": {
+    "Level 51's Winter 2015 Turn↑TheHeat": {
+      "level": 50,
+      "evs": {
+        "hp": "252",
+        "df": "4",
+        "sa": "44",
+        "sd": "20",
+        "sp": "188"
+      },
+      "nature": "Modest",
+      "ability": "Solar Power",
+      "item": "Charizardite Y",
+      "moves": [
+        "Heat Wave",
+        "Solar Beam",
+        "Overheat",
+        ""
+      ]
+    }
+  },
+  "Cresselia": {
+    "Level 51's Winter 2015 Crescenda": {
+      "level": 50,
+      "evs": {
+        "hp": "220",
+        "df": "172",
+        "sa": "4",
+        "sd": "92",
+        "sp": "20"
+      },
+      "nature": "Calm",
+      "ability": "Levitate",
+      "item": "",
+      "moves": [
+        "Psyshock",
+        "Ice Beam",
+        "",
+        ""
+      ]
+    }
+  },
+  "Mamoswine": {
+    "Level 51's Winter 2015 SWINE FLU++": {
+      "level": 50,
+      "evs": {
+        "at": "252",
+        "sd": "4",
+        "sp": "252"
+      },
+      "nature": "Adamant",
+      "ability": "Thick Fat",
+      "item": "Choice Scarf",
+      "moves": [
+        "Icicle Crash",
+        "Earthquake",
+        "Iron Head",
+        "Rock Slide"
+      ]
+    }
+  },
+  "Ferrothorn": {
+    "Level 51's Winter 2015 Free Hugs": {
+      "level": 50,
+      "evs": {
+        "hp": "252",
+        "at": "148",
+        "sd": "108"
+      },
+      "ivs": {
+        "sp": "0"
+      },
+      "nature": "Relaxed",
+      "ability": "",
+      "item": "Leftovers",
+      "moves": [
+        "Gyro Ball",
+        "Power Whip",
+        "",
+        ""
+      ]
+    }
+  },
   "Kangaskhan": {
-    "Level 51's #BANKANGA’15": {
+    "Level 51's Winter 2015 #BANKANGA’15": {
       "level": 50,
       "evs": {
         "hp": "212",
